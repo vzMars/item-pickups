06ffccdbcf3064c60fb81a9ccd4eae545eeb2940
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-04 16:22:20 -0500

    added database connection to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/database');
 
 require('dotenv').config({ path: './config/.env' });
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
