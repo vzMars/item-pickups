8ffbfd17a6c789304fe88115934a9da08b702200
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-04 17:14:36 -0500

    server basic setup

--- server.js
+++ server.js
@@ -1,10 +1,69 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('express-flash');
+const morgan = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./config/database');
+const mainRoutes = require('./routes/main');
 
+// Load config
 require('dotenv').config({ path: './config/.env' });
 
+// Passport config
+require('./config/passport')(passport);
+
+// Database Connection
 connectDB();
+
+// EJS
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+// Static folder
+app.use(express.static('public'));
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// Logging
+app.use(morgan('dev'));
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URI }),
+  })
+);
+
+// Express flash
+app.use(flash());
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/', mainRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
