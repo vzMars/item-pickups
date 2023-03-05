459dab3bc3559aeaccd7f232fdc10e8ef6280327
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-04 20:32:56 -0500

    can signup users (can also display any errors with signing up) and logout users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports = {
   getLogin: async (req, res) => {
     res.render('login', { user: req.user, title: 'Login' });
@@ -5,13 +7,27 @@ module.exports = {
   postLogin: async (req, res) => {
     res.json('login');
   },
-  logout: async (req, res) => {
-    res.json('logout');
+  logout: async (req, res, next) => {
+    req.logout((err) => {
+      if (err) return next(err);
+      req.session.destroy();
+      res.redirect('/');
+    });
   },
   getSignup: (req, res) => {
     res.render('signup', { user: req.user, title: 'Signup' });
   },
-  postSignup: async (req, res) => {
-    res.json('signup');
+  postSignup: async (req, res, next) => {
+    const { username, email, password } = req.body;
+    try {
+      const user = await User.signup(username, email, password);
+      req.logIn(user, (err) => {
+        if (err) return next(err);
+        res.redirect('/');
+      });
+    } catch (error) {
+      req.flash('error', error.message);
+      return res.redirect('/signup');
+    }
   },
 };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -16,6 +18,38 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// static signup method
+UserSchema.statics.signup = async function (username, email, password) {
+  if (!username || !email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  if (!validator.isLength(username, { min: 3, max: 20 })) {
+    throw Error(`Username is too ${username.length < 3 ? 'short' : 'long'}`);
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid');
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  }
+
+  const existingUser = await this.findOne({ $or: [{ username }, { email }] });
+
+  if (existingUser) {
+    throw Error('Account with that email or username already in use');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ username, email, password: hash });
+
+  return user;
+};
 
 // Helper method for validating user's password.
 UserSchema.methods.comparePassword = async function (password) {
