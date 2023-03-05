d437e76b1d439deb65e8d3746a9bac341dfda59b
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-04 20:29:52 -0500

    fixed issue with mongoose function no longer accepting callbacks

--- config/passport.js
+++ config/passport.js
@@ -1,37 +1,43 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
+const customFields = {
+  usernameField: 'email',
+};
+
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy(
-      { usernameField: 'email' },
-      async (email, password, done) => {
-        try {
-          const user = await User.findOne({ email });
+    new LocalStrategy(customFields, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
 
-          if (!user) {
-            return done(null, false, { msg: 'Incorrect email' });
-          }
-
-          const isMatch = await user.comparePassword(password);
-
-          if (!isMatch) {
-            return done(null, false, { msg: 'Incorrect password' });
-          }
-
-          return done(null, user);
-        } catch (err) {
-          return done(err);
+        if (!user) {
+          return done(null, false, { msg: 'Incorrect email' });
         }
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) {
+          return done(null, false, { msg: 'Incorrect password' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
-    )
+    })
   );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
   });
 };
