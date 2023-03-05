f6199870a91a4e725daf986c35efa5ced071afd7
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-05 14:22:21 -0500

    created auth middleware for public and private routes

--- routes/main.js
+++ routes/main.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 router.get('/', homeController.getIndex);
+router.get('/logout', ensureAuth, authController.logout);
+
+router.use(ensureGuest);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
-router.get('/logout', authController.logout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
 
