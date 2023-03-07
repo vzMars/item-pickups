7ed0da9ae8fa78b48833044fcff37b3c64083384
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-06 22:26:09 -0500

    user is able to add a item to the database

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,3 +1,6 @@
+const cloudinary = require('../middleware/cloudinary');
+const Item = require('../models/Item');
+
 module.exports = {
   getExplore: (req, res) => {
     res.render('explore', { user: req.user, title: 'ItemPickups' });
@@ -10,5 +13,29 @@ module.exports = {
   },
   addForm: (req, res) => {
     res.render('add', { user: req.user, title: 'Add Item' });
+  },
+  addItem: async (req, res) => {
+    const { title, itemType, description } = req.body;
+    try {
+      if (!title || !itemType || !description || !req.file) {
+        throw Error('All fields must be filled');
+      }
+
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      await Item.create({
+        title,
+        itemType,
+        description,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        user: req.user.id,
+      });
+
+      res.redirect('/profile');
+    } catch (error) {
+      req.flash('error', error.message);
+      return res.redirect('/item/add');
+    }
   },
 };
--- routes/item.js
+++ routes/item.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const upload = require('../middleware/multer');
 
-router.get('/add', ensureAuth, itemController.addForm);
+router.use(ensureAuth);
+router.get('/add', itemController.addForm);
+router.post('/add', upload.single('file'), itemController.addItem);
 
 module.exports = router;
