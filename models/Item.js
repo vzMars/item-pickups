98c2f001d46bb49bfa393b28833e228d37523427
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-06 21:44:46 -0500

    created item model

---
+++ models/Item.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    itemType: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    cloudinaryId: {
+      type: String,
+      require: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: [],
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Item', ItemSchema);
