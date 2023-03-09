bd4fee4d9f4c7c2b141c89b141dc0a1ad662737d
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-03-08 22:22:38 -0500

    added support to webp images

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,9 +5,13 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-      cb(new Error('File type is not supported'), false);
-      return;
+    if (
+      ext !== '.jpg' &&
+      ext !== '.jpeg' &&
+      ext !== '.png' &&
+      ext !== '.webp'
+    ) {
+      return cb(new Error('File type is not supported'), false);
     }
     cb(null, true);
   },
